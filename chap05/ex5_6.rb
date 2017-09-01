515c735e7f20612968113922ed607ce0800de085
Jon Sanders <jonsanders101@aol.com>
2017-09-01 18:26:57 +0100

    Completed Chapt 6

--- chap05/ex5_6.rb
+++ chap05/ex5_6.rb
@@ -1,11 +1,12 @@
-print "Good afternoon! What's your first name? : "
+print "Good afternoon! What's your first name? :"
 first_name = gets.chomp
-puts = "And your middle name? :"
+print "And your middle name? :"
 middle_name = gets.chomp
-print = "And your last name? :"
+print "And your last name? :"
 last_name = gets.chomp
 
 puts "A pleasure to meet you, " + first_name + " " + middle_name + " " + last_name +"."
 print "Now, " + first_name + ", what is your favourite number? :"
-fav_num = gets.to_i.chomp
-puts fav_num + "? Hmm...that's a good number. Can I recommend " + (fav_num + 1) + "? It's bigger and better."
+fav_num = gets.chomp.to_i
+bigger_num = fav_num + 1
+puts fav_num.to_s + "? Hmm...that's a good number. Can I recommend " + bigger_num.to_s + "? It's bigger and better."
